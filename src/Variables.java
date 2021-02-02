1337e83d5b67b959dfe13d62e3d75899ef0f9317
Smulli <78427713+Smulli@users.noreply.github.com>
2021-02-03 00:43:13 +0100

    Ejercicio 1 UD4.

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+class Variables {
+	
+	public static void main (String args[]) {
+		
+		int N = 3;
+		double A = 4.56;
+		 
+		System.out.println("El resultado de N, A y caracter es:"); //Muestro un mensaje de aviso.
+		System.out.println(N+A); //resultado de la suma.
+		System.out.println(N-A); //resultado de la resta.	
+		System.out.println(N*A); //resultado de la multiplicación.
+		System.out.println(N/A); //resultado de la división.	
+		System.out.println(N%A); //resultado de la división.
+	}
+	
+}
+
+		
